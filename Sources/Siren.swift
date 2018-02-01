781218ae47c723006f318040e7b7457495633e34
Lukas Gauster <lg@chatgrape.com>
2018-02-01 14:20:50 +0100

    changed localization strings for de / en and added a biweekly option to check for updates (3 days)

--- Sources/Siren.swift
+++ Sources/Siren.swift
@@ -504,6 +504,9 @@ public extension Siren {
 
         /// Version check performed once a day.
         case daily = 1
+        
+        /// Version check performed every three days.
+        case biweekly = 3
 
         /// Version check performed once a week.
         case weekly = 7
